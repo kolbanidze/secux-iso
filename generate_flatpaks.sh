d35ffd5e870faa0a1cfae0c3f8a0e0cbc041febd
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2025-02-19 19:20:40 +0300

    cleanup

--- generate_flatpaks.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-flatpak create-usb flatpaks org.chromium.Chromium org.mozilla.firefox ru.yandex.Browser org.telegram.desktop org.videolan.VLC org.keepassxc.KeePassXC org.onlyoffice.desktopeditors org.libreoffice.LibreOffice
