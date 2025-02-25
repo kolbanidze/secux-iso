86a52f054048e6870c900f0c4e2e74a3a400f257
Kolbanidze <137103175+kolbanidze@users.noreply.github.com>
2025-02-25 22:00:45 +0300

    idk

--- make.py
+++ make.py
@@ -1,63 +1,92 @@
 #!/usr/bin/python3
 import os
+import sys
 from datetime import datetime
+import subprocess
 
 WORKDIR = os.path.dirname(os.path.abspath(__file__))
-
 OFFLINE_REPO_PATH = "/var/cache/pacman/offline-repo"
 
+def run_command(command):
+    result = subprocess.run(command, shell=True, check=True)
+    return result.returncode
+
 def cleanup():
-    if os.path.isdir(f"{WORKDIR}/bin"):
-        os.system(f"rm -rf {WORKDIR}/bin/*")
-    else:
-        os.system(f"mkdir -p {WORKDIR}/bin")
-    
-    if not os.path.isdir(f"{WORKDIR}/builds"):
-        os.system(f"mkdir -p {WORKDIR}/builds")
+    try:
+        if os.path.isdir(f"{WORKDIR}/bin"):
+            run_command(f"rm -rf {WORKDIR}/bin/*")
+        else:
+            run_command(f"mkdir -p {WORKDIR}/bin")
+        
+        if not os.path.isdir(f"{WORKDIR}/builds"):
+            run_command(f"mkdir -p {WORKDIR}/builds")
+    except subprocess.CalledProcessError:
+        print("Error during cleanup. Aborting.")
+        sys.exit(1)
 
 def build(offline: bool, refresh_offline_repo: bool = True):
-    if os.path.isdir(f"{WORKDIR}/releng/airootfs/usr/local/share/secux-apps"):
-        os.system("rm -rf {WORKDIR}/releng/airootfs/usr/local/share/secux-apps")
-    if os.path.isdir(f"{WORKDIR}/releng/airootfs/usr/local/share/secux-installer"):
-        os.system("rm -rf {WORKDIR}/releng/airootfs/usr/local/share/secux-installer")
-    if os.path.isdir(f"{WORKDIR}/releng/airootfs/usr/local/share/kirt-app"):
-        os.system("rm -rf {WORKDIR}/releng/airootfs/usr/local/share/kirt-app")
-
-    os.system(f"git clone --depth=1 https://github.com/kolbanidze/secux-installer.git {WORKDIR}/releng/airootfs/usr/local/share/secux-installer")
-    os.system(f"git clone --depth=1 https://github.com/kolbanidze/secux-apps.git {WORKDIR}/releng/airootfs/usr/local/share/secux-apps")
-    os.system(f"git clone --depth=1 https://github.com/kirt-king/test_app.git {WORKDIR}/releng/airootfs/usr/local/share/kirt-app")
-
-    os.system(f"touch {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/production.conf")
-    if offline:
-        os.system(f"touch {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/offline_installation.conf")
-        if refresh_offline_repo or not os.path.isdir(f"{WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo"):
-            os.system(f"rm -rf {WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo")
-            os.system(f"rsync -aAXHv --info=progress2 {OFFLINE_REPO_PATH} {WORKDIR}/releng/airootfs/var/cache/pacman/")
-        os.system(f"cp {WORKDIR}/releng/airootfs/etc/pacman_offline.conf {WORKDIR}/releng/airootfs/etc/pacman.conf")
-    else:
-        os.system(f"cp {WORKDIR}/releng/airootfs/etc/pacman_online.conf {WORKDIR}/releng/airootfs/etc/pacman.conf")
-        os.system(f"rm -rf {WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo/*")
-        if os.path.isfile(f"{WORKDIR}/releng/airootfs/usr/local/share/secux-installer/offline_installation.conf"):
-            os.system(f"rm {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/offline_installation.conf")
-
-    if offline:
-        os.system(f"./{WORKDIR}/releng/airootfs/usr/local/share/secux-installer/collect_python_packages.sh {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/python_packages")
-    
-    os.system(f"mkarchiso -v -w {WORKDIR}/bin -o {WORKDIR}/bin {WORKDIR}/releng")
-    buildtype = "offline" if offline else "online"
-    os.system(f"mv {WORKDIR}/bin/*.iso {WORKDIR}/builds/SecuxLinux-{buildtype}-{datetime.today().strftime("%Y-%m-%d_%H-%M")}.iso")
-
+    try:
+        paths_to_remove = [
+            "secux-apps",
+            "secux-installer",
+            "kirt-app"
+        ]
+        
+        for path in paths_to_remove:
+            full_path = f"{WORKDIR}/releng/airootfs/usr/local/share/{path}"
+            if os.path.isdir(full_path):
+                run_command(f"rm -rf {full_path}")
+        
+        run_command(f"mkdir -p {WORKDIR}/releng/airootfs/var/cache/pacman")
+        
+        repos = {
+            "secux-installer": "https://github.com/kolbanidze/secux-installer.git",
+            "secux-apps": "https://github.com/kolbanidze/secux-apps.git",
+            "kirt-app": "https://github.com/kirt-king/test_app.git"
+        }
+        
+        for repo, url in repos.items():
+            run_command(f"git clone --depth=1 {url} {WORKDIR}/releng/airootfs/usr/local/share/{repo}")
+            
+        run_command(f"touch {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/production.conf")
+        
+        if offline:
+            run_command(f"touch {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/offline_installation.conf")
+            if refresh_offline_repo or not os.path.isdir(f"{WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo"):
+                run_command(f"rm -rf {WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo")
+                run_command(f"rsync -aAXHv --info=progress2 {OFFLINE_REPO_PATH} {WORKDIR}/releng/airootfs/var/cache/pacman/")
+            run_command(f"cp {WORKDIR}/releng/airootfs/etc/pacman_offline.conf {WORKDIR}/releng/airootfs/etc/pacman.conf")
+        else:
+            run_command(f"cp {WORKDIR}/releng/airootfs/etc/pacman_online.conf {WORKDIR}/releng/airootfs/etc/pacman.conf")
+            run_command(f"rm -rf {WORKDIR}/releng/airootfs/var/cache/pacman/offline-repo/*")
+            offline_conf = f"{WORKDIR}/releng/airootfs/usr/local/share/secux-installer/offline_installation.conf"
+            if os.path.isfile(offline_conf):
+                run_command(f"rm {offline_conf}")
+        
+        if offline:
+            run_command(f"chmod +x {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/collect_python_packages.sh")
+            run_command(f"bash {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/collect_python_packages.sh {WORKDIR}/releng/airootfs/usr/local/share/secux-installer/python_packages")
+        
+        run_command(f"mkarchiso -v -w {WORKDIR}/bin -o {WORKDIR}/bin {WORKDIR}/releng")
+        buildtype = "offline" if offline else "online"
+        run_command(f"mv {WORKDIR}/bin/*.iso {WORKDIR}/builds/SecuxLinux-{buildtype}-{datetime.today().strftime('%Y-%m-%d_%H-%M')}.iso")
+    except subprocess.CalledProcessError:
+        print("Error during build process. Aborting.")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if os.geteuid() != 0:
         print("Please run as root!")
-        exit(1)
+        sys.exit(1)
+    
+    # cleanup()
+    # build(offline=False)
     cleanup()
-    build(offline=False)
-    cleanup()
+    
     if os.path.isdir(OFFLINE_REPO_PATH):
         build(offline=True, refresh_offline_repo=True)
     else:
         print("ERROR. Offline repo wasn't found on device!")
-        exit(1)
+        sys.exit(1)
+    
     cleanup()
